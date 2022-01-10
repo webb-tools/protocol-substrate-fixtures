72a767687f490861487c7ce3b5c278dd876e02fa
Filip Lazovic <filiplazovic@Filips-MacBook-Pro-2.local>
2022-01-10 13:48:51 +0100

    Added script for fixtures generation

--- builder/src/main.rs
+++ builder/src/main.rs
@@ -1,58 +1,56 @@
-use std::fmt::Display;
-use std::fs::File;
+use ark_ec::PairingEngine;
+use arkworks_circuits::setup::common::{setup_keys, setup_keys_unchecked};
+use arkworks_circuits::setup::anchor::AnchorProverSetup;
+use arkworks_circuits::setup::mixer::MixerProverSetup;
+use arkworks_utils::utils::common::{setup_params_x5_3, setup_params_x5_5, setup_params_x5_4, Curve};
+use ark_std::test_rng;
+use ark_bn254::Bn254;
+use std::fs::write;
+use std::env::current_dir;
 
-use arkworks_gadgets::ark_std::fmt::{Formatter, Result};
-use arkworks_gadgets::prelude::{ark_bls12_381, ark_bn254, ark_std};
-use arkworks_circuits::setup::{
-	common::setup_tree_and_create_path_tree_x5,
-	mixer::{
-		setup_groth16_random_circuit_x5
-	},
-};
-use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
-use arkworks_utils::{utils::common::Curve};
-use std::io::Write;
+pub const N: usize = 30;
+pub const M: usize = 2;
+type AnchorProverSetupBn254_30<F> = AnchorProverSetup<F, M, N>;
+type MixerProverSetupBn254_30<F> = MixerProverSetup<F, N>;
 
-const TREE_DEPTH:usize  =30;
+fn generate_anchor_keys<E: PairingEngine>(curve: Curve, path: &str) {
+	let mut rng = test_rng();
+	let params3 = setup_params_x5_3::<E::Fr>(curve);
+	let params4 = setup_params_x5_4::<E::Fr>(curve);
 
+	let prover = AnchorProverSetupBn254_30::new(params3, params4);
+	let (circuit, ..) = prover.setup_random_circuit(&mut rng).unwrap();
 
-pub fn get_keys(curve: Curve, pk_bytes: &mut Vec<u8>, vk_bytes: &mut Vec<u8>, pk_uncompressed_bytes: &mut Vec<u8>) {
-	let rng = &mut ark_std::test_rng();
-	match curve {
-		Curve::Bn254 => {
-			let (pk, vk) = setup_groth16_random_circuit_x5::<_, ark_bn254::Bn254, TREE_DEPTH>(rng, curve);
-			vk.serialize(vk_bytes).unwrap();
-			pk.serialize(pk_bytes).unwrap();
-			CanonicalSerialize::serialize_uncompressed(&pk, pk_uncompressed_bytes).unwrap();
-		}
-		Curve::Bls381 => {
-			let (pk, vk) = setup_groth16_random_circuit_x5::<_, ark_bls12_381::Bls12_381, TREE_DEPTH>(rng, curve);
-			vk.serialize(vk_bytes).unwrap();
-			pk.serialize(pk_bytes).unwrap();
-			CanonicalSerialize::serialize_uncompressed(&pk, pk_uncompressed_bytes).unwrap();
-		}
-	};
+	let (pk, vk) = setup_keys::<E, _, _>(circuit.clone(), &mut rng).unwrap();
+	let (pk_unchecked, vk_unchecked) = setup_keys_unchecked::<E, _, _>(circuit, &mut rng).unwrap();
+
+	write(format!("{}/proving_key.bin", path), pk).unwrap();
+	write(format!("{}/verifying_key.bin", path), vk).unwrap();
+	write(format!("{}/proving_key_unchecked.bin", path), pk_unchecked).unwrap();
+	write(format!("{}/verifying_key_unchecked.bin", path), vk_unchecked).unwrap();
 }
 
-pub fn write_keys(pk: &[u8], vk: &[u8], pk_u: &[u8]) {
-	let mut verifying_key = File::create("./fixtures/verifying_key.bin").unwrap();
-	let mut proving_key = File::create("./fixtures/proving_key.bin").unwrap();
-	let mut proving_key_compressed = File::create("./fixtures/proving_key_uncompresed.bin").unwrap();
-	verifying_key.write_all(&vk).unwrap();
-	proving_key.write_all(&pk).unwrap();
-	proving_key_compressed.write_all(&pk_u).unwrap();
-	print!("Wrote files")
-}
+fn generate_mixer_keys<E: PairingEngine>(curve: Curve, path: &str) {
+	let mut rng = test_rng();
+	let params3 = setup_params_x5_3::<E::Fr>(curve);
+	let params5 = setup_params_x5_5::<E::Fr>(curve);
 
-pub fn generate_and_wire_keys(curve: Curve) {
-	let mut pk = Vec::new();
-	let mut pk_u = Vec::new();
-	let mut vk = Vec::new();
-	get_keys(curve, &mut pk, &mut vk, &mut pk_u);
-	write_keys(&pk, &vk, &pk_u);
+	let prover = MixerProverSetupBn254_30::new(params3, params5);
+	let (circuit, ..) = prover.setup_random_circuit(&mut rng).unwrap();
+
+	let (pk, vk) = setup_keys::<E, _, _>(circuit.clone(), &mut rng).unwrap();
+	let (pk_unchecked, vk_unchecked) = setup_keys_unchecked::<E, _, _>(circuit, &mut rng).unwrap();
+
+	write(format!("{}/proving_key.bin", path), pk).unwrap();
+	write(format!("{}/verifying_key.bin", path), vk).unwrap();
+	write(format!("{}/proving_key_unchecked.bin", path), pk_unchecked).unwrap();
+	write(format!("{}/verifying_key_unchecked.bin", path), vk_unchecked).unwrap();
 }
 
 fn main() {
-	/// mixer x5
-	generate_and_wire_keys(Curve::Bn254)
+	let current_path = current_dir().unwrap();
+	let anchor_path = format!("{}/../fixed-anchor/bn254/x5", current_path.display());
+	generate_anchor_keys::<Bn254>(Curve::Bn254, &anchor_path);
+	let mixer_path = format!("{}/../mixer/bn254/x5", current_path.display());
+	generate_mixer_keys::<Bn254>(Curve::Bn254, &mixer_path);
 }
